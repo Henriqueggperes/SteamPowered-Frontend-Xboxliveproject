3c02c7b1b913b421cd5ca9c9c3831830a5336eaa
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-06-30 20:05:49 -0300

    delete: Removed header code

--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -1,30 +1,14 @@
 import * as S from "./style";
 import {ReactComponent as Logo} from './../../assets/img/logo_steam.svg'
+import GamesList from "components/GamesList";
+import Header from "components/Header";
 const HomePage = () => { 
   return (
     <S.Home>
-      <S.HomeHeader> {/*HEADER*/}
-         <S.HomeHeaderImgContainer>
-           <Logo/>
-         </S.HomeHeaderImgContainer>
-           <S.HomeHeaderProfileCardContainer>
-            
-            <S.HomeHeaderProfileCard>  {/* Container que contém o mini card do perfil*/}
-              
-              <S.CardProfileImage>IMAGEM</S.CardProfileImage>
-              
-              <S.CardProfileContainer> {/*Mini card do perfil*/}
-                <S.CardProfileName>Frenchie</S.CardProfileName>
-                <S.CardProfileAdminSettings>Admin settings</S.CardProfileAdminSettings>
-              </S.CardProfileContainer> {/*Mini card do perfil*/}
-                <S.CardProfileCurrencyContainer>
-                   <S.CardProfileCurrency>R$: 1,00</S.CardProfileCurrency>
-                </S.CardProfileCurrencyContainer>
-            
-            </S.HomeHeaderProfileCard> {/* Container que contém o mini card do perfil*/}
-           </S.HomeHeaderProfileCardContainer>
-      </S.HomeHeader>{/*HEADER*/}
-
+      <Header></Header>
+      <S.HomeContentContainer>
+        <GamesList></GamesList>
+      </S.HomeContentContainer>
     </S.Home>
   );
 };
