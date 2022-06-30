2d66f28577ba173e29210da00fe23825d765ef3e
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-06-30 20:44:47 -0300

    delete: icon import

--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -1,5 +1,4 @@
 import * as S from "./style";
-import {ReactComponent as Logo} from './../../assets/img/logo_steam.svg'
 import GamesList from "components/GamesList";
 import Header from "components/Header";
 const HomePage = () => { 
