fbfd142290ae75974756ddfce789b7958a2016be
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-07-04 12:49:42 -0300

    feat: Dinamic games/genres list

--- app/src/components/GamesList/index.tsx
+++ app/src/components/GamesList/index.tsx
@@ -1,18 +1,38 @@
 import * as S from "./style";
+import { games } from "mocks/games";
+import { genres } from "mocks/genres";
 const GamesList = () => {
   return (
     <>
-      <S.FavoritesGenresListContainer>{/*FAVORTITOS*/}
+      <S.FavoritesGenresListContainer>
+        {/*FAVORTITOS*/}
         <S.FavoritesGenresTitle>
           <S.FavoritesGenresTitle>FAVORITOS</S.FavoritesGenresTitle>
         </S.FavoritesGenresTitle>
+        <S.GamesGenresContainer>
+          {games.map((game) => (
+            <S.GameGenreCard>
+              
+              <span>{game.gameName}</span>
+            </S.GameGenreCard>
+          ))}
+        </S.GamesGenresContainer>
       </S.FavoritesGenresListContainer>
 
-     <S.FavoritesGenresListContainer> {/*GENEROS*/}
-     <S.FavoritesGenresTitle>
+      <S.FavoritesGenresListContainer>
+        {/*GENEROS*/}
+        <S.FavoritesGenresTitle>
           <S.FavoritesGenresTitle>GÊNEROS</S.FavoritesGenresTitle>
         </S.FavoritesGenresTitle>
-     </S.FavoritesGenresListContainer>
+        <S.GamesGenresContainer>
+            {genres.map((genre) => (
+              <S.GameGenreCard>
+                
+                <span>{genre.genreName}</span>
+              </S.GameGenreCard>
+            ))}
+        </S.GamesGenresContainer>
+      </S.FavoritesGenresListContainer>
     </>
   );
 };
