f1df2c8196898d7cefe00bfffe2ddcf26bbc06ac
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-07-04 12:53:26 -0300

    feat: game/genre types

---
+++ app/src/utils/types/games.types.ts
@@ -0,0 +1,10 @@
+export type Game = {
+    id: string,
+    gameName: string,
+    imbdScore: string,
+    genre: string,
+    trailerUrl:string,
+    gameplayUrl:string,
+    description:string,
+    image:string
+}
